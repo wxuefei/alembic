6659afc646ac154dae6d28cde5693231919dc0bd
wuxuefei <wuxuefei@dc.jyx.com>
2023-09-15 15:55:21 +0800

    1. separated write function as api

--- bin/AbcWriter/AbcWriter.cpp
+++ bin/AbcWriter/AbcWriter.cpp
@@ -3,13 +3,6 @@
 // Copyright(c) 2023-2024, PASSION PAINT ANIMATION
 //
 //-*****************************************************************************
-
-#include <iostream>
-#include <sstream>
-#include <algorithm>
-//#include "util.h"
-//#include "MayaUtility.h"
-
 #include <string>
 #include <vector>
 #include <Alembic/AbcGeom/All.h>
@@ -30,17 +23,47 @@ using namespace Alembic::AbcCoreFactory;
 
 //-*****************************************************************************
 //-*****************************************************************************
-// DO IT.
-//-*****************************************************************************
-//-*****************************************************************************
-int main(int argc, char* argv[]) {
-    if (argc < 0) {
-        cerr << "Usage: abcwriter outFile.abc \n\n"
-                "Options:\n"
-                "  -v      Verbose for the IObject.\n" ;
-        return -1;
+std::map<int, OCurvesSchema*> oSchemas;
+std::vector<OCurves*> oCurvess;
+OXform *xform = NULL;
+TimeSamplingPtr tsp;
+int writeFrame(int no, std::vector<Alembic::Abc::V3f> points) {
+    // 创建曲线对象
+    OCurvesSchema* oSchema = NULL;
+    if(oSchemas.count(no) > 0)
+        oSchema = oSchemas[no];
+    if (!oSchema) {
+        char name[128];
+        sprintf(name, "curve%d", no);
+        OCurves* curves = new OCurves(*xform, name, tsp);
+        oSchema = &curves->getSchema();        
+        oSchemas[no] = oSchema;
+        oCurvess.push_back(curves);
     }
+    // 添加曲线数据
+    OCurvesSchema::Sample oSamp;
+    int32_t numCurves = 1;
+    std::vector<Alembic::Util::int32_t> nVertices(numCurves);
+    nVertices[0] = 100;
+    oSamp.setCurvesNumVertices(Alembic::Abc::Int32ArraySample(nVertices));
+    oSamp.setBasis(Alembic::AbcGeom::kBsplineBasis);
+    std::vector<Alembic::Util::uint8_t> orders(numCurves);
+    int degree = 2; // xgen always is 2
+    orders[0] = degree + 1; //degree + 1;
+    oSamp.setOrders(Alembic::Abc::UcharArraySample(orders));
+    oSamp.setType(kVariableOrder);
+    int numPoints = points.size();
+    oSamp.setPositions(Alembic::Abc::P3fArraySample(reinterpret_cast<const Imath::V3f*>(points.data()), numPoints));
+    oSamp.setSelfBounds(Alembic::Abc::Box3d());
+    //oSamp.setInvisible(Alembic::Abc::UIntArraySample());
+    //oSamp.setTopologyVisibility(Alembic::Abc::UIntArraySample());
 
+    //Alembic::AbcGeom::SampleSelector sampleSelector(static_cast<float>(startTime));
+    oSchema->set(oSamp); // , sampleSelector);
+    return 0;
+}
+
+int abcCreate(){
     string fileName = "out.abc"; // argv[1];
 
 
@@ -60,19 +83,10 @@ int main(int argc, char* argv[]) {
     double startTime = 1.0;
     double endTime = 100.0;
 
-    int numPoints = 100;
-    std::vector<Alembic::Abc::V3f> points;
-    points.reserve(numPoints);
-
-    for (int i = 0; i < numPoints; ++i)
-    {
-        double t = static_cast<double>(i) / (numPoints - 1);
-        points.push_back(Alembic::Abc::V3f(static_cast<float>(t), 0.0f, 0.0f));
-    }
 
     try {
-        std::vector < chrono_t > samples;
-        samples.push_back( 0.04);
+        //std::vector < chrono_t > samples;
+        //samples.push_back( 0.04);
         //TimeSampling iTs(AbcA::TimeSamplingType(AbcA::TimeSamplingType::kAcyclic), samples);
         TimeSampling iTs(0.04, 1.68);
         TimeSamplingType tst =  iTs.getTimeSamplingType();
@@ -81,35 +95,8 @@ int main(int argc, char* argv[]) {
 
         oArchive.addTimeSampling(iTs);
 
-
-        TimeSamplingPtr tsp(new TimeSampling(iTs));
-        Alembic::AbcGeom::OXform xform(oRoot, "xform_node",tsp);
-
-        // 创建曲线对象
-        //Alembic::AbcGeom::OArchive abcArchive(oArchive, Alembic::Abc::ErrorHandler::kThrowPolicy);
-        Alembic::AbcGeom::OCurves curves(xform, "my_curve", tsp);
-        Alembic::AbcGeom::OCurvesSchema& oSchema = curves.getSchema();
-
-        // 添加曲线数据
-        Alembic::AbcGeom::OCurvesSchema::Sample oSamp;
-        int32_t numCurves = 1;
-        std::vector<Alembic::Util::int32_t> nVertices(numCurves);
-        nVertices[0] = 100;
-        oSamp.setCurvesNumVertices(Alembic::Abc::Int32ArraySample(nVertices));
-        oSamp.setBasis(Alembic::AbcGeom::kBsplineBasis);
-        std::vector<Alembic::Util::uint8_t> orders(numCurves);
-        int degree = 2; // xgen always is 2
-        orders[0] = degree+1; //degree + 1;
-        oSamp.setOrders(Alembic::Abc::UcharArraySample(orders));
-        oSamp.setType(kVariableOrder);
-
-        oSamp.setPositions(Alembic::Abc::P3fArraySample(reinterpret_cast<const Imath::V3f*>(points.data()), numPoints));
-        oSamp.setSelfBounds(Alembic::Abc::Box3d());
-        //oSamp.setInvisible(Alembic::Abc::UIntArraySample());
-        //oSamp.setTopologyVisibility(Alembic::Abc::UIntArraySample());
-
-        //Alembic::AbcGeom::SampleSelector sampleSelector(static_cast<float>(startTime));
-        oSchema.set(oSamp); // , sampleSelector);
+        tsp = TimeSamplingPtr(new TimeSampling(iTs));
+        xform = new OXform(oRoot, "xform_node",tsp);
     }
     catch (const std::exception& e) {
         // 捕获异常并输出错误信息
@@ -118,110 +105,45 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-#if 0
-#include <Alembic/AbcGeom/All.h>
-#include <Alembic/AbcCoreOgawa/All.h>
-#include <Alembic/AbcCoreFactory/All.h>
-//#include <Alembic/AbcCoreHDF5/All.h>
-
-int main()
-{
-    // 创建一个ABC档案文件
-    Alembic::AbcCoreFactory::IFactory factory;
-    Alembic::AbcCoreFactory::IFactory::CoreType coreType = Alembic::AbcCoreFactory::IFactory::kOgawa;
-    Alembic::AbcCoreFactory::IFactory::setCoreType(coreType);
-    Alembic::AbcCoreFactory::IFactory::CoreType currentType = Alembic::AbcCoreFactory::IFactory::getCoreType();
-
-    Alembic::AbcCoreFactory::IFactory::CoreType coreTypes[2] = { Alembic::AbcCoreFactory::IFactory::kHDF5,
-                                                                 Alembic::AbcCoreFactory::IFactory::kOgawa };
-
-    Alembic::AbcCoreFactory::IFactory::ObjectHeader objectHeader;
-    objectHeader.setMetaData(Alembic::AbcCoreFactory::MetaData());
-    Alembic::AbcCoreFactory::IFactory::setCoreType(currentType);
-
-    Alembic::AbcCoreFactory::IFactory::setCoreType(coreTypes[coreType]);
-    Alembic::AbcCoreFactory::IFactory::setOgawaNumStreamsToDisk(2);
-
-    Alembic::AbcCoreFactory::IFactory::setHDF5NumThreads(2);
-    Alembic::AbcCoreFactory::IFactory::setHDF5NumGroupsPerHierarchy(2);
-    Alembic::AbcCoreFactory::IFactory::setHDF5UseSingleFile(false);
-
-    Alembic::Abc::OArchive archive(Alembic::AbcCoreFactory::IFactory::getCoreType(), "curve_example.abc", objectHeader, Alembic::Abc::ErrorHandler::kThrowPolicy);
-
-    // 创建一个时间范围
-    double startTime = 1.0;
-    double endTime = 100.0;
-
-    // 创建曲线的示例数据
+void test() {
+    abcCreate();
     int numPoints = 100;
     std::vector<Alembic::Abc::V3f> points;
     points.reserve(numPoints);
 
-    for (int i = 0; i < numPoints; ++i)
-    {
-        double t = static_cast<double>(i) / (numPoints - 1);
-        points.push_back(Alembic::Abc::V3f(static_cast<float>(t), 0.0f, 0.0f));
+    for (int i = 0; i < numPoints; ++i) {
+        double x = (double)i / (numPoints);
+        points.push_back(Alembic::Abc::V3f(x, 0.0f, 0.0f));
     }
-
-    // 创建曲线对象
-    Alembic::AbcGeom::OCurves curves(archive.getTop(), "my_curve", 1);
-    Alembic::AbcGeom::OCurvesSchema& curvesSchema = curves.getSchema();
-
-    // 添加曲线数据
-    Alembic::AbcGeom::OCurvesSchema::Sample curvesSample;
-    curvesSample.setPositions(Alembic::Abc::P3fArraySample(reinterpret_cast<const Imath::V3f*>(points.data()), numPoints));
-    curvesSample.setSelfBounds(Alembic::Abc::Box3d());
-    curvesSample.setInvisible(Alembic::Abc::UIntArraySample());
-    curvesSample.setTopologyVisibility(Alembic::Abc::UIntArraySample());
-
-    Alembic::AbcGeom::SampleSelector sampleSelector(static_cast<float>(startTime));
-    curvesSchema.set(curvesSample, sampleSelector);
-
-    // 关闭存档
-    archive.getArchive().getErrorHandler().cleanup();
-
-    return 0;
-}
-#elif 0
-#include <Alembic/AbcGeom/All.h>
-#include <Alembic/AbcCoreOgawa/All.h>
-
-int main()
-{
-    // 创建一个 ABC 存档文件（使用 Ogawa 文档类型）
-    Alembic::AbcCoreOgawa::WriteArchive archive = Alembic::AbcCoreOgawa::WriteArchive(); // ("curve_example.abc");
-
-    // 创建一个时间范围
-    double startTime = 1.0;
-    double endTime = 100.0;
-
-    // 创建曲线的示例数据
-    int numPoints = 100;
-    std::vector<Alembic::Abc::V3f> points;
-    points.reserve(numPoints);
-
-    for (int i = 0; i < numPoints; ++i)
-    {
-        double t = static_cast<double>(i) / (numPoints - 1);
-        points.push_back(Alembic::Abc::V3f(static_cast<float>(t), 0.0f, 0.0f));
+    writeFrame(1, points);
+    for (int i = 0; i < numPoints; ++i) {
+        points[i][1] += 0.1;
     }
+    writeFrame(1, points);
+    for (int i = 0; i < numPoints; ++i) {
+        points[i][1] += 0.1;
+    }
+    writeFrame(2, points);
 
-    // 创建曲线对象
-    Alembic::AbcGeom::OArchive abcArchive(archive, Alembic::Abc::ErrorHandler::kThrowPolicy);
-    Alembic::AbcGeom::OCurves curves(abcArchive.getTop(), "my_curve", 1);
-    Alembic::AbcGeom::OCurvesSchema& curvesSchema = curves.getSchema();
+    for (int i = 0; i < oCurvess.size(); i++)
+        if (oCurvess[i])
+            delete oCurvess[i];
+    if (xform)
+        delete xform;
 
-    // 添加曲线数据
-    Alembic::AbcGeom::OCurvesSchema::Sample curvesSample;
-    curvesSample.setPositions(Alembic::Abc::P3fArraySample(reinterpret_cast<const Imath::V3f*>(points.data()), numPoints));
-    curvesSample.setSelfBounds(Alembic::Abc::Box3d());
-    //curvesSample.setInvisible(Alembic::Abc::UIntArraySample());
-    //curvesSample.setTopologyVisibility(Alembic::Abc::UIntArraySample());
-
-    //Alembic::AbcGeom::SampleSelector sampleSelector(static_cast<float>(startTime));
-    curvesSchema.set(curvesSample); // , sampleSelector);
-
-    return 0;
 }
 
-#endif
+//-*****************************************************************************
+// main
+//-*****************************************************************************
+int main(int argc, char* argv[]) {
+    if (argc < 0) {
+        cerr << "Usage: abcwriter outFile.abc \n\n"
+            "Options:\n"
+            "  -v      Verbose for the IObject.\n";
+        return -1;
+    }
+    test();
+
+    printf("done\n");
+}
